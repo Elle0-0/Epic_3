ac650104aa0042a6077e45f71dd8873c3773edc6
Darragh Luby <23359048@studentmail.ul.ie>
2024-03-14 17:29:44 +0000

    Update main.py to include updated country names in combined dataset

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from pandas import DataFrame
 
 from modules import co2_totals, surface_temperatures
-from modules.similarity import show_similar_countries
+from modules.misc import get_similar_countries
 
 FILE_NAME: str = "combined-data.csv"
 PATH_OUT: str = f"../datasets/clean/{FILE_NAME}"
@@ -22,9 +22,12 @@ def main() -> None:
 
     df1_countries: set[str] = set(df1["Country"])
     df2_countries: set[str] = set(df2["Country"])
+    similar_countries: dict[str, str] = get_similar_countries(df1_countries, df2_countries)
 
-    print("\nSimilar Countries:")
-    show_similar_countries(df1_countries, df2_countries, similarity_threshold=0.7)
+    print(f"\nNormalizing following country names:\n{similar_countries}")
+
+    # Normalize similar country names so they are the same
+    df1["Country"].replace(similar_countries, inplace=True)
 
     # Only counts rows with matching countries
     combined: DataFrame = pd.merge(df1, df2, on="Country", suffixes=("_co2_total", "_surface_temperature"))
--- preprocessing/modules/misc.py
+++ preprocessing/modules/misc.py
@@ -2,14 +2,15 @@ from typing import Optional
 
 from difflib import SequenceMatcher
 
+SIMILARITY_THRESHOLD: float = 0.73
 
-def most_similar_country(country: str, countries: tuple[str], *, similarity_threshold: float = 0.6) -> Optional[str]:
+
+def most_similar_country(country: str, countries: tuple[str]) -> Optional[str]:
     """
     Finds the most similar match given a country and a tuple of countries to match from.
 
     :param country: Country name.
     :param countries: Tuple of countries to find similar match from.
-    :param similarity_threshold: Minimum measure of "similarity" – a float between [0, 1].
 
     :return: None if there are no sufficient matches, otherwise the most
              similar match greater than or equal to the given threshold.
@@ -19,7 +20,7 @@ def most_similar_country(country: str, countries: tuple[str], *, similarity_thre
 
     for _country in countries:
         similarity: float = SequenceMatcher(isjunk=None, a=country, b=_country).ratio()
-        if similarity >= similarity_threshold:
+        if similarity >= SIMILARITY_THRESHOLD:
             matches[similarity] = _country
 
     if not matches:
@@ -28,26 +29,24 @@ def most_similar_country(country: str, countries: tuple[str], *, similarity_thre
     return matches[max(matches)]
 
 
-def show_similar_countries(countries_a: set[str], countries_b: set[str], *, similarity_threshold: float = 0.6) -> None:
+def get_similar_countries(countries_a: set[str], countries_b: set[str]) -> dict[str, str]:
     """
-    Displays the most similar country in the second set of countries for
-    each country in the first set of countries. Ignores equal strings.
+    Maps each country in the first set of countries to the most similar
+    country in the second set of countries and returns as a dictionary.
 
     :param countries_a: First set of countries.
     :param countries_b: Second set of countries.
-    :param similarity_threshold: see `most_similar_country.__doc__`
     """
 
+    similarities: dict[str, str] = {}
     countries_b_unique: tuple[str] = tuple(country for country in countries_b if country not in countries_a)
 
     for country in countries_a:
         if country not in countries_b:
 
-            corresponding_country: str | None = most_similar_country(
-                country,
-                countries_b_unique,
-                similarity_threshold=similarity_threshold
-            )
+            corresponding_country: str | None = most_similar_country(country, countries_b_unique)
 
             if corresponding_country is not None:
-                print(f"    Dataframe 1: {country}".ljust(50), f"Dataframe 2: {corresponding_country}")
+                similarities[country] = corresponding_country
+
+    return similarities
